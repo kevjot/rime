b92a0cc60308150af3876d95da745a5c46d0bd16
vaporstroll <vaporstroll@gmail.com>
2023-05-16 17:24:39 +0800

    update

--- lua/date_translator.lua
+++ lua/date_translator.lua
@@ -2,7 +2,7 @@ local function date_translator(input, seg)
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d"), ""))
     end
     if (input == "time") then
